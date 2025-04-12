ce9706f24ae540a3402b6d1252d8fcaf65d45958
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-04-12 23:37:23 +0545

    first class

---
+++ day1.py
@@ -0,0 +1,19 @@
+# welcome to our class beta
+#open a terminal you use : ctr + ~
+# print("my name is yuresh gurung")  
+# # Write a program to input student name and marks of 3 subjects. Print name and percentage in output.
+# for i in range(3):
+# 	studname = input("Enter a name: ")
+# 	Marks = input("Enter a  marks:")
+	
+	
+# average = (int(Marks) + int(Marks) + int(Marks)) / 3
+# print("The average is: ", average)
+
+# ctl+ / all comment one short
+
+
+# function
+def add(a, b):
+	return a + b
+print
